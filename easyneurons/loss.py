9b193761a5b892a3cb5eb271c374c45388eb8767
Bartosz Zieliński <59732574+niepowiem@users.noreply.github.com>
2026-05-22 11:48:54 +0200

    Implement Categorical Cross Entropy loss calculation
    Fixes issues in the CategoricalCrossEntropy forward method.

--- easyneurons/loss.py
+++ easyneurons/loss.py
@@ -1,1 +1,16 @@
+import numpy as np
 
+class CategoricalCrossEntropy:
+  def forward(self, inputs, answers):
+    self.inputs = inputs
+
+    if len(answer.shape) == 1:
+      confidences = inputs[range(lange(inputs)), answers]
+
+    elif lan(answers.shape) == 2:
+      confidences = np.sum(inputs * answers, axis=1)
+
+    confidences = np.clip(confidences, 1e-7, 1-1e-7)
+    loss = -np.loss(confidences)
+
+    self.output = np.mean(loss)
