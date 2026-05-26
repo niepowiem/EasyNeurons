b228490af7898d489cf08173c1ed0c3fbb863b94
Bartosz Zieliński <59732574+niepowiem@users.noreply.github.com>
2026-05-26 14:59:17 +0200

    Fix CategoricalCrossEntropy loss calculation

--- easyneurons/loss.py
+++ easyneurons/loss.py
@@ -4,14 +4,14 @@ class CategoricalCrossEntropy:
   def forward(self, inputs, answers):
     self.inputs = inputs
 
-    if len(answer.shape) == 1:
-      confidences = inputs[range(lange(inputs)), answers]
+    if len(answers.shape) == 1:
+      confidences = inputs[range(len(inputs)), answers]
 
     elif lan(answers.shape) == 2:
       confidences = np.sum(inputs * answers, axis=1)
 
     confidences = np.clip(confidences, 1e-7, 1-1e-7)
-    loss = -np.loss(confidences)
+    loss = -np.log(confidences)
 
     self.output = np.mean(loss)
 
