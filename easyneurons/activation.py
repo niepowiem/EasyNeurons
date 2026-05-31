49fb4a3c6f84b5ea2280da0e3cb1c94867bec9a4
Bartosz Zieliński <59732574+niepowiem@users.noreply.github.com>
2026-05-31 21:41:26 +0200

    Refactored classes and added extensive docs for ReLU

--- easyneurons/activation.py
+++ easyneurons/activation.py
@@ -2,22 +2,128 @@ import numpy as np
 
 class ReLU:
     """
-    ENG:
-    This function is supposed to be put on top of neuronal outputs of hidden layers. This will add nonlinearity to the neural network. For more watch:
+    Rectified Linear Unit (ReLU) activation function
+    ------------------------------------------------
 
-    PL:
-    Ta funkcja ma być nakładana na wyniki neuronów ukrytych warstw. Wprowadzi to nieliniowość do sieci neuronowej. Wyjaśnienie:
+    This class introduces non-linearity by zeroing out negative
+    values while leaving positive values unchanged.
+
+    Mathematical formula
+    --------------------
+    f(x) = { x > 0: x, x < 0: 0 }
+
+    Good For
+    --------
+    TBC
+
+    Performance
+    -----------
+    TBC
+
+    Troubleshooting
+    ---------------
+    1. Dying ReLU problem: Occurs when neurons persist in returning zero,
+    thus resulting in zero gradient, which leads to neuron never activating
+    and being unable to learn.
+
+    Fixing the dying ReLU problem is essential becuse if many neurons die,
+    the neural network's ability to learn complex data drops. It can slow down
+    training and reduce performance.
+
+    Diagnosis: The learning rate stays unchanged, even after couple of epochs
+
+    Cause:
+    a) Weight Initialization: If weights are set in a way that makes the neurons outputs
+    mainly negative values, after passing through the activation functions, these
+    neurons may become permatetly inactive.
+
+    b) LARGE negative biases: Even when the weight is positive, a large negative
+    bias, after being added to w * x, can lead to negative output. Despite having
+    a valid input and positive weight, the large negative bias may lead to the
+    neuron outputting values below zero. If such biases persist across many neurons,
+    they can cause these neurons to become inactive.
+
+    c) Too HIGH Learning Rate: When learning rate is too high it can update parameters
+    with large negative values. For example:
+    dinput = 10
+    weight = 0.5
+    learning_rate = 1
+    w_new = w - learning_rate * dinput = 0.5 - 1 * 10 = -9.5
+
+    Solutions:
+    a) **Using LeakyReLU**, which allows non-zero gradient for negative values,
+    ensuring the neurons maintain some level of activity, even when the input
+    is negative.
+    b) **Exploring other activation functions** such as: PReLU, ELU
+    c) **Lower learning rates** can prevent weights from becoming excessively negative,
+    after updates, which reduces the risk of neurons returning negative values
+    d) **Proper initialization** can ensure, the neurons start
+    with values more likely to keep them active in the early stages of learning
+    e) **Batch Normalization** TBC
+
+    2. Unbounded Output: Because ReLU is unbounded on the positive side, this
+    can lead to gradients growing exponentially, reaching astronomical values
+    thus destabilizing the model. Common issue in RNN.
+
+    Diagnosis:
+        a) Loss function suddenly jumps to absurdly high values
+        b) During training, loss function returns 'NaN'
+        c) Weights are 'inf'
+
+    Solutions:
+    a) **Gradient Clipping** is used for exploding gradients problem. This method
+    clips the graients to be in a certain threshold for example to be between -4 and 4.
+    This method has its drawback, because by clipping the gradient, the vector direction
+    may change. To maintain the direction of the vector, we can **Clip By Norm**, which
+    means, instead of only clipping values outside of the range, we are lowering all of
+    our gradient values to be in betweeen -1 and 1. The drawback of this method is that
+    some of the vales may become very small.
+    b) **Proper initialization** TBC
+    c) **Batch Normalization** TBC
+
+    3. Noisy Gradients:
+
+    Video Explanation
+    -----------
+    ENG: ccc
+    PL: ccc
+
+    Attributes
+    ----------
+    input : np.ndarray
+        The input passed to the forward method.
+    output : np.ndarray
+        The result after applying the activation function.
     """
-
-    def forward(self, inputs):
+    def forward(self, inputs: np.ndarray) -> np.ndarray:
         """
-        This function is setting any number of inputs that is less than 0 to 0
-
-        :param inputs: Matrix of neuronal outputs
-        :return:
+        TBC
         """
         self.input = inputs
         self.output = np.maximum(0, inputs)
+
+        return self.output
+
+class LeakyReLU:
+    def forward(self, inputs, alpha=0.01):
+        self.input = inputs
+        self.output = np.maximum(alpha * inputs, inputs)
+
+class PReLU:
+    def forward(self, inputs, alpha=0.01):
+        self.input = inputs
+
+class ELU:
+    def forward(self, inputs, alpha=0.01):
+        self.input = inputs
+
+class GELU:
+    def forward(self, inputs, alpha=0.01):
+        self.input = inputs
+
+class SELU:
+    def forward(self, inputs, alpha=0.01):
+        self.input = inputs
 
 class Softmax:
     """
@@ -26,6 +132,7 @@ class Softmax:
 
     PL:
     Ta funkcja zwraca rozkład procentowy z wyników warstwy wyjściowej (tylko jedna odpowiedź). Należy ją stosować tylko na warstwie wyjściowej: Wyjaśnienie:
+    https://www.youtube.com/watch?v=XNcbSeSeMUk
     """
 
     def forward(self, inputs):
@@ -56,16 +163,143 @@ class Sigmoid:
         self.input = inputs
         self.output = 1 / (1 + np.exp(-inputs))
 
+class Linear:
+    def forward(self, inputs):
+        self.input = inputs
+
 class Binary:
     def forward(self, inputs):
         self.output = np.heaviside(inputs, 1)
 
-class LeakyReLU:
+class Swish:
     def forward(self, inputs, alpha=0.01):
-        self.input = inputs 
-        self.output = np.maximum(alpha * x, x)
+        self.input = inputs
+
+class Mish:
+    def forward(self, inputs, alpha=0.01):
+        self.input = inputs
+
+class Sin:
+    def forward(self, inputs, alpha=0.01):
+        self.input = inputs
 
 class Tanh:
     def forward(self, inputs):
         self.input = inputs
         self.output = np.tanh(input)
+
+class Softsign:
+    def forward(self, inputs, alpha=0.01):
+        self.input = inputs
+
+class Softplus:
+    def forward(self, inputs, alpha=0.01):
+        self.input = inputs
+
+class Snake:
+    def forward(self, inputs, alpha=0.01):
+        self.input = inputs
+
+class GLU:
+    def forward(self, inputs, alpha=0.01):
+        self.input = inputs
+
+class ReLUClip:
+    def forward(self, inputs, max=6):
+        self.input = inputs
+
+class HardSigmoid:
+    def forward(self, inputs, alpha=0.01):
+        self.input = inputs
+
+class HardSwish:
+    def forward(self, inputs, alpha=0.01):
+        self.input = inputs
+
+class HardTanh:
+    def forward(self, inputs, alpha=0.01):
+        self.input = inputs
+
+class LogSoftmax:
+    def forward(self, inputs, alpha=0.01):
+        self.input = inputs
+
+class SiLU:
+    def forward(self, inputs, alpha=0.01):
+        self.input = inputs
+
+class CReLU:
+    def forward(self, inputs, alpha=0.01):
+        self.input = inputs
+
+class GumbelSoftmax:
+    def forward(self, inputs, alpha=0.01):
+        self.input = inputs
+
+class Softmin:
+    def forward(self, inputs, alpha=0.01):
+        self.input = inputs
+
+class Threshold:
+    def forward(self, inputs, alpha=0.01):
+        self.input = inputs
+
+class StarReLU:
+    def forward(self, inputs, alpha=0.01):
+        self.input = inputs
+
+class Gaussian:
+    def forward(self, inputs, alpha=0.01):
+        self.input = inputs
+
+class Sinc:
+    def forward(self, inputs, alpha=0.01):
+        self.input = inputs
+
+class ArcTanh:
+    def forward(self, inputs, alpha=0.01):
+        self.input = inputs
+
+class ISRU:
+    def forward(self, inputs, alpha=0.01):
+        self.input = inputs
+
+class BentIdentity:
+    def forward(self, inputs, alpha=0.01):
+        self.input = inputs
+
+class Maxout:
+    def forward(self, inputs, alpha=0.01):
+        self.input = inputs
+
+class SwiGLU:
+    def forward(self, inputs, alpha=0.01):
+        self.input = inputs
+
+class CELU:
+    def forward(self, inputs, alpha=0.01):
+        self.input = inputs
+
+class GeGLU:
+    def forward(self, inputs, alpha=0.01):
+        self.input = inputs
+
+class ReGLU:
+    def forward(self, inputs, alpha=0.01):
+        self.input = inputs
+
+class Abs:
+    def forward(self, inputs, alpha=0.01):
+        self.input = inputs
+
+class Cosine:
+    def forward(self, inputs, alpha=0.01):
+        self.input = inputs
+
+class SQNL:
+    def forward(self, inputs, alpha=0.01):
+        self.input = inputs
+
+class FReLU:
+    def forward(self, inputs, alpha=0.01):
+        self.input = inputs
