0b634ef2c04ec213c1eb5dbc3fcc9952d7916343
Bartosz Zieliński <59732574+niepowiem@users.noreply.github.com>
2026-06-01 12:33:02 +0200

    Add backward method to NLayer class

--- easyneurons/layer.py
+++ easyneurons/layer.py
@@ -31,3 +31,19 @@ class NLayer:
         """
 
         self.output = np.dot(inputs, self.weights) + self.biases
+
+    def backward(self, dvalues: np.ndarray) -> np.ndarray:
+        """
+        TBC
+        :param dvalues:
+        :return:
+        """
+
+        # Gradients on parameters
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+
+        # Gradient on values
+        self.dvalues = np.dot(dvalues, self.weights.T)
+
+        return self.dvalues
