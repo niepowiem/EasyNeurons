cde18dae8351f1144e296c88f2b5d15c41ba043c
Bartosz Zieliński <59732574+niepowiem@users.noreply.github.com>
2026-06-04 21:55:53 +0200

    Enhance NLayer with seed for random weight generation
    Added an optional seed parameter for weight initialization.

--- easyneurons/layer.py
+++ easyneurons/layer.py
@@ -9,13 +9,17 @@ class NLayer:
     Ta klasa to prosta warstwa neuronów. Wyjaśnienie: https://www.youtube.com/watch?v=W-tN-7qrv0k
     """
 
-    def __init__(self, n_inputs, n_outputs):
+    def __init__(self, n_inputs, n_outputs, seed = None):
         """
         :param n_inputs: Number of inputs this layer in requesting
         :param n_outputs: Number of outputs of this layer
         """
 
-        self.weights = 0.01 * np.random.randn(n_inputs,n_outputs)
+        if seed:
+            self.weights = 0.01 * np.random.default_rng(seed=seed).standard_normal((n_inputs,n_outputs))
+        else:
+            self.weights = 0.01 * np.random.randn(n_inputs,n_outputs)
+
         self.biases = np.zeros((1, n_outputs))
 
     def forward(self, inputs):
@@ -30,9 +34,10 @@ class NLayer:
         :return:
         """
 
+        self.inputs = inputs
         self.output = np.dot(inputs, self.weights) + self.biases
 
-    def backward(self, dvalues: np.ndarray) -> np.ndarray:
+    def backward(self, dinputs: np.ndarray) -> np.ndarray:
         """
         TBC
         :param dvalues:
@@ -40,10 +45,10 @@ class NLayer:
         """
 
         # Gradients on parameters
-        self.dweights = np.dot(self.inputs.T, dvalues)
-        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+        self.dweights = np.dot(self.inputs.T, dinputs)
+        self.dbiases = np.sum(dinputs, axis=0, keepdims=True)
 
         # Gradient on values
-        self.dvalues = np.dot(dvalues, self.weights.T)
+        self.dvalues = np.dot(dinputs, self.weights.T)
 
         return self.dvalues
