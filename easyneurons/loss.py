b54d6cc5fcdbe864299c21ad52538b13a6d6e998
Bartosz Zieliński <59732574+niepowiem@users.noreply.github.com>
2026-06-11 00:21:21 +0200

    Refactor CategoricalCrossEntropy methods for clarity

--- easyneurons/loss.py
+++ easyneurons/loss.py
@@ -1,40 +1,34 @@
 import numpy as np
 
 class CategoricalCrossEntropy:
-  def forward(self, inputs, answers):
-    self.inputs = inputs
+    def forward(self, inputs, answers) -> float:
+        self.answers = answers
+        self.inputs = inputs
 
-    if len(answers.shape) == 1:
-      confidences = inputs[range(len(inputs)), answers]
+        if len(answers.shape) == 1:
+            confidences = inputs[range(len(inputs)), answers]
 
-    elif lan(answers.shape) == 2:
-      confidences = np.sum(inputs * answers, axis=1)
+        elif lan(answers.shape) == 2:
+            confidences = np.sum(inputs * answers, axis=1)
 
-    confidences = np.clip(confidences, 1e-7, 1-1e-7)
-    loss = -np.log(confidences)
+        confidences = np.clip(confidences, 1e-7, 1-1e-7)
+        loss = -np.log(confidences)
 
-    self.output = np.mean(loss)
+        self.output = np.mean(loss)
 
-  def backward(self) -> np.ndarray:
-    # Number of samples
-    samples = len(self.inputs)
+    def backward(self) -> np.ndarray:
+        samples = len(self.inputs)
 
-    # Number of labels in every sample
-    # We'll use the first sample to count them
-    labels = len(self.inputs[0])
+        labels = len(self.inputs[0])
 
-    # If labels are sparse, turn them into one-hot vector
-    if len(self.answers.shape) == 1:
-      self.answers = np.eye(labels)[self.answers]
+        if len(self.answers.shape) == 1:
+            self.answers = np.eye(labels)[self.answers]
 
-    # Calculate gradient
-    self.dinputs = -self.answers / self.inputs
-
-    # Normalize gradient
-    self.dinputs = self.dinputs / samples
+        self.dinputs = -self.answers / self.inputs
+        self.dinputs = self.dinputs / samples
 
 class MeanSquaredError:
-  def forward(self, inputs, answers):
-    self.inputs = inputs
+    def forward(self, inputs, answers):
+        self.inputs = inputs
     
-    self.output = np.mean((inputs - answers)**2, axis=-1)
+        self.output = np.mean((inputs - answers)**2, axis=-1)
