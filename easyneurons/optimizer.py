c46cf4e80020cc670ad7669f8da3970910a3d4bf
Bartosz Zieliński <59732574+niepowiem@users.noreply.github.com>
2026-06-12 01:08:42 +0200

    Refactor StochasticGradientDescent and add SGD class

--- easyneurons/optimizer.py
+++ easyneurons/optimizer.py
@@ -1,6 +1,11 @@
 import numpy as np
+from easyneurons import layer
+from easyneurons.base import Model, Dataset
+from easyneurons.layer import *
+from easyneurons.layer import NLayer
+from itertools import batched
 
-class StochasticGradientDescent:
+class StochasticGradientDescent_depricated:
     def __init__(self, learning_rate=1.0, decay=0., momentum = 0.):
         self.learning_rate = learning_rate
         self.current_learning_rate = learning_rate
@@ -39,3 +44,69 @@ class StochasticGradientDescent:
 
     def post_update_params(self):
         self.iterations += 1
+
+class SGD:
+    def __init__(self, model: Model, learning_rate:float | int=1.0, decay:float | int=0., momentum:float | int=0.):
+        self.model = model
+
+        self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+
+        self.decay = decay
+        self.momentum = momentum
+
+        self.iterations = 0
+        self.epochs = 0
+
+    def pre_update_parameters(self):
+        if self.decay:
+            # L/(1+d*x)
+            self.current_learning_rate = self.learning_rate * (1 / (1 + self.iterations * self.decay))
+
+    def update_parameters(self, element: NeuralElement):
+        parameters = element.get_parameters(copy=False)
+
+        if not parameters:
+            return
+
+        updates = { }
+
+        if self.momentum:
+            # Inicjalizacja momentów jesli nie istnieją
+            if not hasattr(element, '_momentums'):
+                element._momentums = {key: np.zeros_like(value) for key, value in parameters.items()}
+
+            for key, value in parameters.items():
+                momentum_update = self.momentum * element._momentums[key] - self.current_learning_rate * getattr(element, f"d{key}")
+                element._momentums[key] = momentum_update
+                updates[key] = value + momentum_update
+
+        else:
+            for key, value in parameters.items():
+                updates[key] = value - self.current_learning_rate * getattr(element, f"d{key}")
+
+        element.set_parameters(updates)
+
+    def post_update_parameters(self):
+        self.iterations += 1
+
+    def train(self, dataset: Dataset, epochs: int, callback=None):
+        for epoch in range(epochs):
+            self.pre_update_parameters()
+
+            results = None
+            answers = None
+
+            for inputs, answers in dataset:
+                results = self.model.forward(inputs, answers)
+                self.model.backward()
+
+                for element in self.model.elements:
+                    self.update_parameters(element)
+
+                self.post_update_parameters()
+
+            if callback:
+                callback(self, results, answers)
+
+            self.epochs += 1
