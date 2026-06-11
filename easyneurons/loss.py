89c7ab20833eef5ddfe6d54ab45ba3c29649448a
Bartosz Zieliński <59732574+niepowiem@users.noreply.github.com>
2026-06-12 01:09:57 +0200

    Refactor loss classes to inherit from Loss base class

--- easyneurons/loss.py
+++ easyneurons/loss.py
@@ -1,20 +1,21 @@
 import numpy as np
+from .base import Loss
 
-class CategoricalCrossEntropy:
-    def forward(self, inputs, answers) -> float:
+class CategoricalCrossEntropy(Loss):
+    def forward(self, inputs: np.ndarray, answers: np.ndarray) -> float | int:
         self.answers = answers
         self.inputs = inputs
 
         if len(answers.shape) == 1:
             confidences = inputs[range(len(inputs)), answers]
 
-        elif lan(answers.shape) == 2:
+        elif len(answers.shape) == 2:
             confidences = np.sum(inputs * answers, axis=1)
 
         confidences = np.clip(confidences, 1e-7, 1-1e-7)
         loss = -np.log(confidences)
 
-        self.output = np.mean(loss)
+        self.outputs = np.mean(loss)
 
     def backward(self) -> np.ndarray:
         samples = len(self.inputs)
@@ -27,8 +28,12 @@ class CategoricalCrossEntropy:
         self.dinputs = -self.answers / self.inputs
         self.dinputs = self.dinputs / samples
 
-class MeanSquaredError:
-    def forward(self, inputs, answers):
+        return self.dinputs
+
+class MeanSquaredError(Loss):
+    def forward(self, inputs: np.ndarray, answers: np.ndarray):
         self.inputs = inputs
     
-        self.output = np.mean((inputs - answers)**2, axis=-1)
+        self.outputs = np.mean((inputs - answers)**2, axis=-1)
+
+        return self.outputs
