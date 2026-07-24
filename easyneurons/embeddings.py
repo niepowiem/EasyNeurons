30bb7b2f7469f0f34f50bb3b210280a468bfbf40
Bartosz Zieliński <59732574+niepowiem@users.noreply.github.com>
2026-07-24 12:54:02 +0200

    Refactor CoOccurrenceMatrix for improved efficiency

--- easyneurons/embeddings.py
+++ easyneurons/embeddings.py
@@ -5,9 +5,12 @@ from pympler import asizeof
 import os
 import struct
 from pathlib import Path
+import heapq
 
 class CoOccurrenceMatrix:
     BYTES_PER_PAIR = 192.0
+    RECORD_FMT = "<iid"
+    RECORD_SIZE = struct.calcsize(RECORD_FMT)
 
     def __init__(self):
         pass
@@ -60,19 +63,51 @@ class CoOccurrenceMatrix:
             shard_filenames.append(self._flush_buffer_to_shard(buffer, shard_directory=shard_directory, shard_idx=len(shard_filenames)))
             buffer.clear()
 
-        
+        out_path = os.path.join(shard_directory, 'merged_cooccurrence.bin')
+        shards = [self._read_shard(f) for f in shard_filenames]
+        with open(out_path, 'wb') as out:
+            current_key, current_value = None, 0.0
+
+            for key, value in heapq.merge(*shards, key=lambda kv: kv[0]):
+                if key == current_key:
+                    current_value += value
+
+                else:
+                    if current_key is not None:
+                        out.write(struct.pack(self.RECORD_FMT, current_key[0], current_key[1], current_value))
+
+                    current_key, current_value = key, value
+
+            if current_key is not None:
+                out.write(struct.pack(self.RECORD_FMT, current_key[0], current_key[1], current_value))
+
+        for f in shard_filenames:
+            os.remove(f)
+
+        return out_path
 
 
 
-
-    @staticmethod
-    def _flush_buffer_to_shard(buffer: dict, shard_directory: str, shard_idx: int) -> str:
+    def _flush_buffer_to_shard(self, buffer: dict, shard_directory: str, shard_idx: int) -> str:
         filename = os.path.join(shard_directory, f'com_shard_{shard_idx:05d}.bin')
         with open(filename, 'wb') as f:
             for (a, b), value in sorted(buffer.items()):
-                f.write(struct.pack("<iid", a, b, value))
+                f.write(struct.pack(self.RECORD_FMT, a, b, value))
 
         return filename
+
+    def _read_shard(self, filename: str, io_buffer_records: int = 4096):
+        chunk_bytes = io_buffer_records * self.RECORD_SIZE
+        with open(filename, 'rb') as f:
+            while True:
+                block = f.read(chunk_bytes)
+                if not block:
+                    break
+
+                for i in range(0, len(block), self.RECORD_SIZE):
+                    a,b,value = struct.unpack(self.RECORD_FMT, block[i:i+self.RECORD_SIZE])
+
+                    yield (a,b), value
 
     def __iter__(self):
         pass
@@ -82,4 +117,5 @@ class GloVe():
 
 if __name__ == '__main__':
     com = CoOccurrenceMatrix()
-    com.calculate([[1,2,3,4,5], [5,4,3,2,1]], sliding_window=3, weighted=True, max_bytes_in_ram=180, check_ram_every_n_tokens=-1)
+    a = com.calculate([[1,2,3,4,5], [5,4,3,2,1]], sliding_window=3, weighted=True, max_bytes_in_ram=192, check_ram_every_n_tokens=-1)
+    print(a)
