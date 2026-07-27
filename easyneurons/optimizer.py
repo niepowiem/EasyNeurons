fd87b84b273c2ec407380829de787e45e934651d
Bartosz Zieliński <59732574+niepowiem@users.noreply.github.com>
2026-07-27 11:21:26 +0200

    Refactor SGD and add AdaGrad optimizer implementation

--- easyneurons/optimizer.py
+++ easyneurons/optimizer.py
@@ -95,3 +95,79 @@ class SGD(Optimizer):
                 metrics.clear()
 
             self.post_update_parameters()
+
+class AdaGrad(Optimizer):
+    parameters = ("epochs", "iterations", "learning_rate", "current_learning_rate", "decay", "epsilon")
+
+    def __init__(self, model: Model, learning_rate: float | int = 1.0, decay: float | int = 0., epsilon: float = 1e-7):
+        self.model = model
+
+        self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+
+        self.decay = decay
+        self.epsilon = epsilon
+
+        self.iterations = 0
+        self.epochs = 0
+
+    def pre_update_parameters(self):
+        if self.decay:
+            # L/(1+d*x)
+            self.current_learning_rate = self.learning_rate * (1 / (1 + self.epochs * self.decay))
+
+    def update_parameters(self, element: NeuralElement):
+        parameters = element.get_trainable()
+
+        if not parameters:
+            return
+
+        updates = { }
+
+        if not hasattr(element, '_cache'):
+            element._cache = {key: np.zeros_like(value) for key, value in parameters.items()}
+
+        for key, value in parameters.items():
+            element._cache[key] = getattr(element, f"d{key}") ** 2
+            updates[key] = value - self.current_learning_rate * getattr(element, f"d{key}") / (np.sqrt(element._cache[key]) + self.epsilon)
+
+        element.set_parameters(updates)
+
+    def post_update_parameters(self):
+        self.epochs += 1
+
+    def train(self, dataset: Dataset, epochs: int, tracker: Tracker = None, metrics: Metrics = None, callback=None):
+        for epoch in range(epochs):
+            self.pre_update_parameters()
+            self.iterations = 0
+
+            # Loss for tacker
+            local_loss = 0
+            for inputs, answers in dataset:
+                results = self.model.forward(inputs, answers)
+
+                self.model.backward()
+                for element in self.model.elements:
+                    self.update_parameters(element)
+
+                local_loss += results["loss"]
+
+                if metrics:
+                    metrics.add(results["output"], answers)
+
+                if callback:
+                    callback({
+                        "results": results,
+                        "answers": answers
+                    })
+
+                self.iterations += 1
+
+            if tracker:
+                tracker.log({"loss": local_loss / self.iterations} | self.get_parameters())
+
+            if metrics:
+                metrics.calculate(force=False)
+                metrics.clear()
+
+            self.post_update_parameters()
