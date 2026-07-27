230241140f427ea4c51d1bd616d04ad85577177b
Bartosz Zieliński <59732574+niepowiem@users.noreply.github.com>
2026-07-27 12:53:26 +0200

    Add weight initialization classes and refactor NLayer
    Refactor NLayer to use initialization classes for weights.

--- easyneurons/layer.py
+++ easyneurons/layer.py
@@ -1,5 +1,69 @@
+from abc import ABC, abstractmethod
+
 import numpy as np
 from easyneurons.general import NeuralElement
+
+class Initialization(ABC):
+
+    @abstractmethod
+    def initialize(self, n_inputs: int, n_outputs: int) -> np.ndarray:
+        pass
+
+class RandomInitialization(Initialization):
+    def __init__(self, mode: str="normal", multiplier: float = 0.01, seed: int = None):
+        if mode not in ('uniform', 'gaussian', 'normal'):
+            raise ValueError("Value of 'at' must be one of 'uniform', 'gaussian', 'normal'")
+
+        self.mode = mode
+        self.multiplier = multiplier
+        self.seed = seed
+
+    def initialize(self, n_inputs: int, n_outputs: int) -> np.ndarray:
+        if self.mode == "uniform":
+            if self.seed:
+                return self.multiplier * np.random.default_rng(seed=seed).uniform(-1, 1, (n_inputs, n_outputs))
+
+            return self.multiplier * np.random.uniform(-1, 1, (n_inputs, n_outputs))
+
+        if self.seed:
+            return self.multiplier * np.random.default_rng(seed=seed).standard_normal((n_inputs, n_outputs))
+
+        return self.multiplier * np.random.standard_normal((n_inputs, n_outputs))
+
+class StaticInitialization(Initialization):
+    def __init__(self, number: float = 0.0):
+        self.number = number
+
+    def initialize(self, n_inputs: int, n_outputs: int) -> np.ndarray:
+        return self.number * np.ones((n_inputs, n_outputs))
+
+# class XavierGlorotInitialization(Initialization):
+#     def __init__(self, seed: int = None):
+#         pass
+# 
+#     def initialize(self, n_inputs: int, n_outputs: int) -> np.ndarray:
+#         pass
+# 
+# class HeKaimingInitialization(Initialization):
+#     def __init__(self, seed: int = None):
+#         pass
+#     
+#     def initialize(self, n_inputs: int, n_outputs: int) -> np.ndarray:
+#         pass
+#     
+# class LeCunInitialization(Initialization):
+#     def __init__(self, seed: int = None):
+#         pass
+#     
+#     def initialize(self, n_inputs: int, n_outputs: int) -> np.ndarray:
+#         pass
+#     
+# class OrthogonalInitialization(Initialization):
+#     def __init__(self, seed: int = None):
+#         pass
+#     
+#     def initialize(self, n_inputs: int, n_outputs: int) -> np.ndarray:
+#         pass
 
 class NLayer(NeuralElement):
     """
@@ -13,17 +77,13 @@ class NLayer(NeuralElement):
     parameters = ("weights", "biases")
     trainable = parameters
 
-    def __init__(self, n_inputs, n_outputs, seed = None):
+    def __init__(self, n_inputs, n_outputs, initialization: Initialization = RandomInitialization()):
         """
         :param n_inputs: Number of inputs this layer in requesting
         :param n_outputs: Number of outputs of this layer
         """
 
-        if seed:
-            self.weights = 0.01 * np.random.default_rng(seed=seed).standard_normal((n_inputs,n_outputs))
-        else:
-            self.weights = 0.01 * np.random.randn(n_inputs,n_outputs)
-
+        self.weights = initialization.initialize(n_inputs=n_inputs, n_outputs=n_outputs)
         self.biases = np.zeros((1, n_outputs))
 
     def forward(self, inputs: np.ndarray) -> np.ndarray:
